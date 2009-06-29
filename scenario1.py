f4683d1c067b9c8538501450d7e45cfa1ebee567
darius <darius@96-41-125-34.dhcp.mtpk.ca.charter.com>
2009-06-28 22:43:15 -0700

    make a VM class, subclassed for each scenario

--- scenario1.py
+++ scenario1.py
@@ -1,11 +1,30 @@
 import vm
 
+def main():
+    s1vm = S1VM(1001)
+    s1vm.load('bin1.obf')
+    s1vm.step()
+
 # Sensor ports
 s_score, s_fuel, s_sx, s_sy, s_tx, s_ty = range(6)
 
 # Actuator ports
 a_dvx, a_dvy, a_config = 2, 3, 0x3E80
 
-vm.load('bin1.obf')
-vm.scenario = 1001.0
-vm.step()
+
+class S1VM(vm.VM):
+
+    def __init__(self, scenario):
+        vm.VM.__init__(self)
+        self.scenario = float(scenario)
+        self.dv = (0.0, 0.0)
+
+    def input(self, actuator):
+        if actuator == a_config: return self.scenario
+        if actuator == a_dvx:    return self.dv[0]
+        if actuator == a_dvy:    return self.dv[1]
+        assert False
+
+
+if __name__ == '__main__':
+    main()
--- vm.py
+++ vm.py
@@ -2,104 +2,128 @@ import math
 import struct
 import sys
 
-scenario = None
-
-insns = []
-data = []
-
-def load(filename):
-    f = open(filename, 'rb')
-    for frame in range(2**14):
-        bytes = f.read(12)
-        if not bytes: break
-        if frame % 2 == 0:
-            d, i = struct.unpack('<dI', bytes)
-        else:
-            i, d = struct.unpack('<Id', bytes)
-        insns.append(i)
-        data.append(d)
-
-status = False
-
 def main():
     assert 2 == len(sys.argv)
-    load(sys.argv[1])
-    disassemble()
+    vm = VM()
+    vm.load(sys.argv[1])
+    vm.disassemble()
 
-def disassemble():
-    for addr, insn in enumerate(insns):
-        show(addr, insn)
+scenario = None
 
-def show(addr, insn):
-    print ('%5d %10g %-30s %08x'
-           % (addr, data[addr], disassemble1(insn), insn))
 
-def step():
-    for pc, insn in enumerate(insns):
-        show(pc, insn)
+class VM:
+
+    def __init__(self):
+        self.clear()
+
+    def clear(self):
+        self.insns = []
+        self.data = []
+        self.status = False
+
+    def load(self, filename):
+        f = open(filename, 'rb')
+        for frame in range(2**14):
+            bytes = f.read(12)
+            if not bytes: break
+            if frame % 2 == 0:
+                d, i = struct.unpack('<dI', bytes)
+            else:
+                i, d = struct.unpack('<Id', bytes)
+            self.insns.append(i)
+            self.data.append(d)
+
+    def disassemble(self):
+        for addr, insn in enumerate(self.insns):
+            self.show(addr, insn)
+
+    def show(self, addr, insn):
+        print ('%5d %10g %-30s %08x'
+               % (addr, self.data[addr], self.disassemble1(insn), insn))
+
+    def step(self):
+        for pc, insn in enumerate(self.insns):
+            self.show(pc, insn)
+            if insn_kind(insn) == 'S':
+                self.do_S(pc, insn)
+            else:
+                self.do_D(pc, insn)
+
+    def disassemble1(self, insn):
         if insn_kind(insn) == 'S':
-            do_S(pc, insn)
+            return self.disassemble_S(insn)
         else:
-            do_D(pc, insn)
+            return self.disassemble_D(insn)
 
-def disassemble1(insn):
-    if insn_kind(insn) == 'S':
-        return disassemble_S(insn)
-    else:
-        return disassemble_D(insn)
+    def disassemble_S(self, insn):
+        op, r1 = decode_S(insn)
+        if op == 0: return 'noop'
+        if op == 1: return self.disassemble_cmp(insn, r1)
+        if op == 2: return 'sqrt M[%d]' % r1
+        if op == 3: return 'M[%d]' % r1
+        if op == 4: return 'read %d' % r1
+        assert False
 
-def disassemble_S(insn):
-    op, r1 = decode_S(insn)
-    if op == 0: return 'noop'
-    if op == 1: return disassemble_cmp(insn, r1)
-    if op == 2: return 'sqrt M[%d]' % r1
-    if op == 3: return 'M[%d]' % r1
-    if op == 4: return 'read %d' % r1
-    assert False
+    def do_S(self, pc, insn):
+        op, r1 = decode_S(insn)
+        if   op == 0: pass
+        elif op == 1: self.do_cmp(insn, r1)
+        elif op == 2: self.update(pc, math.sqrt(self.get(r1)))
+        elif op == 3: self.update(pc, self.get(r1))
+        elif op == 4: self.update(pc, self.input(r1))
+        else:         assert False
 
-def do_S(pc, insn):
-    op, r1 = decode_S(insn)
-    if   op == 0: pass
-    elif op == 1: do_cmp(insn, r1)
-    elif op == 2: update(pc, math.sqrt(get(r1)))
-    elif op == 3: update(pc, get(r1))
-    elif op == 4: update(pc, input(r1))
-    else:         assert False
+    def disassemble_cmp(self, insn, r1):
+        cmpi = field(insn, 23, 20)
+        cmp = '< <= = >= >'.split()[cmpi]
+        return 'status = (M[%d] %s 0.0)' % (r1, cmp)
 
-def disassemble_cmp(insn, r1):
-    cmpi = field(insn, 23, 20)
-    cmp = '< <= = >= >'.split()[cmpi]
-    return 'status = (M[%d] %s 0.0)' % (r1, cmp)
+    def do_cmp(self, insn, r1):
+        cmpi = field(insn, 23, 21)
+        if   cmpi == 0: self.status = (self.get(r1) <  0.0)
+        elif cmpi == 1: self.status = (self.get(r1) <= 0.0)
+        elif cmpi == 2: self.status = (self.get(r1) == 0.0)
+        elif cmpi == 3: self.status = (self.get(r1) >= 0.0)
+        elif cmpi == 4: self.status = (self.get(r1) >  0.0)
+        else: assert False
 
-def do_cmp(insn, r1):
-    global status
-    cmpi = field(insn, 23, 21)
-    if   cmpi == 0: status = (get(r1) <  0.0)
-    elif cmpi == 1: status = (get(r1) <= 0.0)
-    elif cmpi == 2: status = (get(r1) == 0.0)
-    elif cmpi == 3: status = (get(r1) >= 0.0)
-    elif cmpi == 4: status = (get(r1) >  0.0)
-    else: assert False
+    def disassemble_D(self, insn):
+        op, r1, r2 = decode_D(insn)
+        if op == 1: return 'M[%d] + M[%d]' % (r1, r2)
+        if op == 2: return 'M[%d] - M[%d]' % (r1, r2)
+        if op == 3: return 'M[%d] * M[%d]' % (r1, r2)
+        if op == 5: return 'M[%d] / M[%d]' % (r1, r2)
+        if op == 4: return 'write %d <- M[%d]' % (r1, r2)
+        if op == 6: return 'status ? M[%d] : M[%d]' % (r1, r2)
+        assert False
 
-def disassemble_D(insn):
-    op, r1, r2 = decode_D(insn)
-    if op == 1: return 'M[%d] + M[%d]' % (r1, r2)
-    if op == 2: return 'M[%d] - M[%d]' % (r1, r2)
-    if op == 3: return 'M[%d] * M[%d]' % (r1, r2)
-    if op == 5: return 'M[%d] / M[%d]' % (r1, r2)
-    if op == 4: return 'write %d <- M[%d]' % (r1, r2)
-    if op == 6: return 'status ? M[%d] : M[%d]' % (r1, r2)
-    assert False
+    def do_D(self, pc, insn):
+        op, r1, r2 = decode_D(insn)
+        if   op == 1: self.update(pc, self.get(r1) + self.get(r2))
+        elif op == 2: self.update(pc, self.get(r1) - self.get(r2))
+        elif op == 3: self.update(pc, self.get(r1) * self.get(r2))
+        elif op == 4: self.update(pc, self.get(r1) / self.get(r2)
+                                  if self.get(r2) != 0.0
+                                  else 0.0)
+        elif op == 5: self.output(r1, self.get(r2))
+        elif op == 6: self.update(pc, self.get(r1)
+                                  if self.status
+                                  else self.get(r2))
+        else:         assert False
 
-def do_D(pc, insn):
-    op, r1, r2 = decode_D(insn)
-    if   op == 1: update(pc, get(r1) + get(r2))
-    elif op == 2: update(pc, get(r1) - get(r2))
-    elif op == 3: update(pc, get(r1) * get(r2))
-    elif op == 4: update(pc, get(r1) / get(r2) if get(r2) != 0.0 else 0.0)
-    elif op == 5: output(r1, get(r2))
-    elif op == 6: update(pc, get(r1) if status else get(r2))
-    else:         assert False
+    def get(self, addr):
+        return self.data[addr] if addr < len(self.data) else 0.0
+
+    def update(self, pc, value):
+        self.data[pc] = value
+        print 'set %d = %g' % (pc, value)
+
+    def output(self, sensor, value):
+        print 'sensor %d <- %g' % (sensor, value)
+
+    def input(self, actuator):
+        return 0.0
+
 
 def insn_kind(insn):
     return 'S' if field(insn, 31, 28) == 0 else 'D'
@@ -113,23 +137,6 @@ def decode_D(insn):
 def field(u32, hi, lo):
     return (u32 >> lo) & ((1 << (hi + 1 - lo)) - 1)
 
-def get(addr):
-    if addr < len(data):
-        return data[addr]
-    return 0.0
-
-def update(pc, value):
-    data[pc] = value
-    print 'set %d = %g' % (pc, value)
-
-def output(sensor, value):
-    print 'sensor %d <- %g' % (sensor, value)
-
-def input(actuator):
-    # XXX hook this up to the scenario properly
-    if actuator == 0x3E80:
-        return scenario
-    return 0.0
 
 if __name__ == '__main__':
     main()
