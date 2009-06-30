8a2ee49b655af4358e0f74e27d7a702020aa07d3
darius <darius@96-41-125-34.dhcp.mtpk.ca.charter.com>
2009-06-30 05:09:03 -0700

    add compiled vm wrapper

---
+++ compiledvm.py
@@ -0,0 +1,47 @@
+import ctypes
+import struct
+
+import bin1
+
+# XXX add code to make sure this is big enough
+SensorsArray   = ctypes.c_double * 50
+ActuatorsArray = ctypes.c_double * 16001 # XXX wasteful
+StatusArray    = ctypes.c_int * 1
+
+
+class CompiledVM:
+
+    def __init__(self, bin_name, loud=False):
+        data_module = __import__(bin_name)
+        DataArray   = ctypes.c_double * len(data_module.data)
+        self.lib       = ctypes.CDLL('lib%s.dylib' % bin_name)
+        self.data      = DataArray(*data_module.data)
+        self.sensors   = SensorsArray()
+        self.actuators = ActuatorsArray()
+        self.status    = StatusArray(0)
+        self.updated   = set()
+        self.nsteps    = 0
+        self.trace     = []
+
+    def write_trace(self, trace_file, team_id, scenario):
+        def write(*args):
+            trace_file.write(struct.pack(*args))
+        write('<III', 0xCAFEBABE, team_id, scenario)
+        for step, acts in self.trace:
+            write('<II', step, len(acts))
+            for a_id, value in acts:
+                write('<Id', a_id, value)
+        write('<II', self.nsteps, 0)
+
+    def step(self):
+        self.lib.step(self.data, self.sensors, self.actuators, self.status)
+        if self.updated:
+            acts = [(a, self.actuators[a]) for a in sorted(self.updated)]
+            self.trace.append((self.nsteps, acts))
+            self.updated.clear()
+        self.nsteps += 1
+
+    def actuate(self, actuator, value):
+        value = float(value)
+        self.updated.add(actuator)
+        self.actuators[actuator] = value
